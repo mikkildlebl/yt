1726e0f799dd1795cab920e19ad4c1450bb813d7
mikkildlebl <47360155+mikkildlebl@users.noreply.github.com>
2023-10-01 14:10:28 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+import json
 
 app = Flask(__name__)
 
+recent_videos = []
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return render_template("index.html")
+
+@app.route('/', methods = ['POST']) 
+def get_data(): 
+    link = request.form['link']
+
+    if request.method == 'POST': 
+        #with open('data.txt', 'w') as f:
+        #     f.write("\n" + str(link))
+        #     f.close()
+        pass
+
+    recent_videos.append(link)
+    print(recent_videos)
+    items = len(recent_videos)
+    if items > 19:
+        print(items)
+        
+    return render_template("index.html", link=link, recent_videos=recent_videos) 
